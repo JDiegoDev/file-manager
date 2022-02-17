07216403bc2c197fd1a84379d86febe120ff7a3e
Juan Diego Gimenez Pena <diego.gimenez.dev@gmail.com>
2022-02-17 10:13:11 -0600

    Adding body parser to handle requests from client

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({
+	extended: true
+  }));
+  
+app.use(bodyParser.json());
+
 require("./services/fileDocs")(app);
 
 /*
